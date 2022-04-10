ab180a8de8b06c0f3977cf135dc31cfbb8672c96
201250184-Lin Haoran <201250184@smail.nju.edu.cn>
2022-04-10 15:15:49 +0800

    implement find_in_stack

--- lab2/Code/stack.c
+++ lab2/Code/stack.c
@@ -28,3 +28,38 @@ Table pop(Stack stack) {
     stack->top--;
     return stack->tables[stack->top];
 }
+
+bool both(FieldList f) {
+    return true;
+}
+
+bool isVar(FieldList f) {
+    return f->is_var;
+}
+
+bool isStructDef(FieldList f) {
+    return !f->is_var;
+}
+
+FieldList find_helper(Stack stack, const char* name, bool (*filter)(FieldList)) {
+    for (int current = stack->top - 1; current >= 0; current--) {
+        FieldList target = find_field(stack->tables[current], name);
+        if (target != NULL && filter(target)) {
+            return target;
+        }
+    }
+
+    return NULL;
+}
+
+FieldList find_any_in_stack(Stack stack, const char* name) {
+    return find_helper(stack, name, both);
+}
+
+FieldList find_var_in_stack(Stack stack, const char* name) {
+    return find_helper(stack, name, isVar);
+}
+
+FieldList find_struct_def_in_stack(Stack stack, const char* name) {
+    return find_helper(stack, name, isStructDef); 
+}
