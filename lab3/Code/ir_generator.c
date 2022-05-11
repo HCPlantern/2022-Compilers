58da767fa852650f6526701dd6a1718550c3a419
None <None>
2022-05-11 21:53:01 +0800

    add ir_generator

---
+++ lab3/Code/ir_generator.c
@@ -0,0 +1,147 @@
+#include "ir_generator.h"
+#include "ir.h"
+
+static inline void set_int_const(Node* node, int i) {
+    node->is_constant = true;
+    node->constant.i = i;
+}
+
+static inline void set_float_const(Node* node, float f) {
+    node->is_constant = true;
+    node->constant.f = f;
+}
+
+void assign_gen(Node* father, Node* lValue, Node* exp) {
+    // TODO
+    // check if the last ir's lValue is exp->var_in_ir
+    // if true, just modify the last ir,
+    // else gen a new ir.
+}
+
+void not_gen(Node* father, Node* exp) {
+    if (exp->is_constant) {
+        if (exp->type.u.basic == T_INT) {
+            assert(exp->type.kind == BASIC);
+            set_int_const(father, exp->constant.i ? 0 : 1);
+        }
+        if (exp->type.u.basic == T_FLOAT) {
+            assert(exp->type.kind == BASIC);
+            set_int_const(father, exp->constant.f ? 0 : 1);
+        }
+        return;
+    }
+    /*
+    else {
+        if (exp->type.u.basic == T_INT) {
+            strncpy(father->var_in_ir, get_temp_var(0), 10);
+        }
+        else {
+            strncpy(father->var_in_ir, get_temp_var(1), 10);
+        }
+    }
+    */
+}
+
+void and_gen(Node* father, Node* exp1, Node* exp2) {
+    // TODO
+}
+
+void or_gen(Node* father, Node* exp1, Node* exp2) {
+    // TODO
+}
+
+void relop_gen(Node* father, Node* exp1, Node* exp2) {
+    // TODO
+}
+
+void plus_gen(Node* father, Node* exp1, Node* exp2) {
+    if (exp1->is_constant && exp2->is_constant) {
+        father->is_constant = true;
+        if (exp1->type.u.basic == T_INT) {
+            father->constant.i = exp1->constant.i + exp2->constant.i;
+        }
+        else {
+            father->constant.f = exp1->constant.f + exp2->constant.f;
+        }
+        return;
+    }
+    
+    father->is_constant = false;
+    char ir[100];
+    strncpy(father->var_in_ir, get_temp_var(0), 10);
+    if (exp1->is_constant) {
+        sprintf(ir, "%s := #%d + %s",
+            father->var_in_ir, exp1->constant.i, exp2->var_in_ir);
+    }
+    else if (exp2->is_constant){
+        sprintf(ir, "%s := %s + #%d",
+            father->var_in_ir, exp1->var_in_ir, exp2->constant.i);
+    }
+    else {
+        sprintf(ir, "%s := %s + %s",
+            father->var_in_ir, exp1->var_in_ir, exp2->var_in_ir);
+    }
+    add_last_code(ir);
+}
+
+void minus_gen(Node* father, Node* exp1, Node* exp2) {
+    // TODO
+}
+
+void star_gen(Node* father, Node* exp1, Node* exp2) {
+    // TODO
+}
+
+void div_gen(Node* father, Node* exp1, Node* exp2) {
+    // TODO
+}
+
+void negative_gen(Node* father, Node* exp) {
+    // TODO
+}
+
+void parathesis_reduce(Node* exp) {
+    // TODO
+}
+
+void negative_gen(Node* father, Node* exp) {
+    // TODO
+}
+
+// void tilde_gen(Node* exp); // vm does not support tilde.
+
+// special process for read() and write(x)
+void func_call_gen(Node* father, Node* id, Node* args) {
+    // TODO
+}
+
+void array_store_gen(Node* father, Node* array, Node* index) {
+    // TODO
+}
+
+void array_load_gen(Node* father, Node* array, Node* index) {
+    // TODO
+}
+
+void field_store_gen(Node* father, Node* base, Node* field) {
+    // TODO
+}
+
+void field_load_gen(Node* father, Node* base, Node* field) {
+    // TODO
+}
+
+void id_gen(Node* father, Node* id) {
+    char* ir_var = get_ir_var_by_name(id->data.text);
+    father->is_constant = false;
+    father->addr_offset = 0;
+    strncpy(father->var_in_ir, ir_var, 15);
+}
+
+void int_gen(Node* father, Node* int_literal) {
+    set_int_const(father, int_literal->data.i);
+}
+
+void float_gen(Node* father, Node* float_literal) {
+    set_float_const(father, float_literal->data.f);
+}
---
+++ lab3/Code/ir_generator.h
@@ -0,0 +1,27 @@
+#ifndef IR_GENERATOR_H
+#define IR_GENERATOR_H
+#include "node.h"
+
+void assign_gen(Node* father, Node* lValue, Node* exp);
+void not_gen(Node* father, Node* exp);
+void and_gen(Node* father, Node* exp1, Node* exp2);
+void or_gen(Node* father, Node* exp1, Node* exp2);
+void relop_gen(Node* father, Node* exp1, Node* exp2);
+void plus_gen(Node* father, Node* exp1, Node* exp2);
+void minus_gen(Node* father, Node* exp1, Node* exp2);
+void star_gen(Node* father, Node* exp1, Node* exp2);
+void div_gen(Node* father, Node* exp1, Node* exp2);
+void negative_gen(Node* father, Node* exp);
+void parathesis_reduce(Node* exp);
+void negative_gen(Node* father, Node* exp);
+// void tilde_gen(Node* exp); // vm does not support tilde.
+void func_call_gen(Node* father, Node* id, Node* args); // special process for read() and write(x)
+void array_store_gen(Node* father, Node* array, Node* index);
+void array_load_gen(Node* father, Node* array, Node* index);
+void field_store_gen(Node* father, Node* base, Node* field);
+void field_load_gen(Node* father, Node* base, Node* field);
+void id_gen(Node* father, Node* id);
+void int_gen(Node* father, Node* _int);
+void float_gen(Node* father, Node* _float);
+
+#endif
